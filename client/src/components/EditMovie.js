3f7fc55fe537f0e9aa5d55f4aa6f0200ca7af4df
KAY S <KS.KWANRUAN@GMAIL.com>
2021-07-25 23:57:00 +1000

    reusable components

--- client/src/components/EditMovie.js
+++ client/src/components/EditMovie.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import './EditMovie.css';
+import Input from './form-components/Input';
+import TextArea from './form-components/TextArea';
 
 export default class EditMovie extends Component {
   state = {
@@ -8,14 +10,43 @@ export default class EditMovie extends Component {
     error: null,
   };
 
-  componentDidMount() {
-    this.setState({
+  constructor(props) {
+    super(props);
+    this.state = {
       movie: {
-        title: 'The Godfather',
-        mpaa_rating: 'R',
+        id: 0,
+        title: '',
+        release_date: '',
+        runtime: '',
+        mpaa_rating: '',
+        rating: '',
+        description: '',
       },
-    });
+      isLoaded: false,
+      error: null,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleSubmit = (evt) => {
+    console.log('Form was submitted');
+    evt.preventDefault();
+  };
+
+  handleChange = (evt) => {
+    let value = evt.target.value;
+    let name = evt.target.name;
+    this.setState((prevState) => ({
+      movie: {
+        ...prevState.movie,
+        [name]: value,
+      },
+    }));
+  };
+
+  componentDidMount() {}
 
   render() {
     let { movie } = this.state;
@@ -24,51 +55,49 @@ export default class EditMovie extends Component {
       <Fragment>
         <h2>Add/Edit Movie</h2>
         <hr />
-        <form method="post">
-          <div className="mb-3">
-            <label for="title" className="form-label">
-              Title
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="title"
-              name="title"
-              value={movie.title}
-            />
-          </div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="hidden"
+            name="id"
+            id="id"
+            value={movie.id}
+            onChange={this.handleChange}
+          />
+
+          <Input
+            title={'Title'}
+            type={'text'}
+            name={'title'}
+            value={movie.title}
+            handleChange={this.handleChange}
+          />
+
+          <Input
+            title={'Release Date'}
+            type={'date'}
+            name={'release_date'}
+            value={movie.release_date}
+            handleChange={this.handleChange}
+          />
+
+          <Input
+            title={'Runtime'}
+            type={'text'}
+            name={'runtime'}
+            value={movie.runtime}
+            handleChange={this.handleChange}
+          />
 
           <div className="mb-3">
-            <label for="release_date" className="form-label">
-              Release date
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="release_date"
-              name="release_date"
-              value={movie.release_date}
-            />
-          </div>
-
-          <div className="mb-3">
-            <label for="runtime" className="form-label">
-              Runtime
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="runtime"
-              name="runtime"
-              value={movie.runtime}
-            />
-          </div>
-
-          <div className="mb-3">
-            <label for="mpaa_rating" className="form-label">
+            <label htmlFor="mpaa_rating" className="form-label">
               MPAA Rating
             </label>
-            <select className="form-select" value={movie.mpaa_rating}>
+            <select
+              name="mpaa_rating"
+              className="form-select"
+              value={movie.mpaa_rating}
+              onChange={this.handleChange}
+            >
               <option className="form-select">Choose...</option>
               <option className="form-select" value="G">
                 G
@@ -76,7 +105,7 @@ export default class EditMovie extends Component {
               <option className="form-select" value="PG">
                 PG
               </option>
-              <option className="form-select" value="PG14">
+              <option className="form-select" value="PG13">
                 PG13
               </option>
               <option className="form-select" value="R">
@@ -88,37 +117,29 @@ export default class EditMovie extends Component {
             </select>
           </div>
 
-          <div className="mb-3">
-            <label for="rating" className="form-label">
-              Rating
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="rating"
-              name="rating"
-              value={movie.rating}
-            />
-          </div>
-
-          <div className="mb-3">
-            <label for="description" className="form-label">
-              Description
-            </label>
-            <textarea
-              className="form-control"
-              id="description"
-              name="description"
-              rows="3"
-            >
-              {movie.description}
-            </textarea>
-          </div>
+          <Input
+            title={'Rating'}
+            type={'text'}
+            name={'rating'}
+            value={movie.rating}
+            handleChange={this.handleChange}
+          />
+          <TextArea
+            title={'Description'}
+            name={'description'}
+            value={movie.description}
+            rows={'3'}
+            handleChange={this.handleChange}
+          />
 
           <hr />
 
           <button className="btn btn-primary">Save</button>
         </form>
+
+        <div className="mt-3">
+          <pre>{JSON.stringify(this.state, null, 3)}</pre>
+        </div>
       </Fragment>
     );
   }
